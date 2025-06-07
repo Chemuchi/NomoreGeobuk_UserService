9c6da37fe62235b514e6ca9e071c2456c0b4e6c7
ash_hyeong <chamchi0129@gmail.com>
2025-06-08 03:39:13 +0900

    feat:상세로그 및 기능 추가

--- config/db.go
+++ config/db.go
@@ -12,11 +12,11 @@ var DB *sql.DB
 
 func InitDB() {
 	// 환경변수 로드
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	name := os.Getenv("DB_NAME")
+	host := os.Getenv("USERSERVICE_DB_HOST")
+	port := os.Getenv("USERSERVICE_DB_PORT")
+	user := os.Getenv("USERSERVICE_DB_USER")
+	password := os.Getenv("USERSERVICE_DB_PASSWORD")
+	name := os.Getenv("USERSERVICE_DB_NAME")
 
 	DCS := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, name)
@@ -24,8 +24,47 @@ func InitDB() {
 	var err error
 	DB, err = sql.Open("postgres", DCS)
 	if err != nil {
-		panic("DB 연결 안됨: " + err.Error())
+		panic("[DB 연결 안됨: " + err.Error() + "]")
 	}
 
-	log.Println("[ DB 설정 성공. ]")
+	// 연결 테스트
+	log.Println("[DB 연결 테스트 중입니다..]")
+	err = DB.Ping()
+	if err != nil {
+		panic("[DB 연결 테스트 실패: " + err.Error() + "]")
+	}
+
+	log.Println("[DB 연결이 성공적으로 완료되었습니다.]")
+
+	checkTable(DB)
+}
+
+func checkTable(db *sql.DB) {
+	query := `SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users');`
+
+	var exists bool
+	err := db.QueryRow(query).Scan(&exists)
+	if err != nil {
+		log.Fatalf("[users 데이블 존재 여부 확인 실패 : %v]", err)
+	}
+
+	if !exists {
+		log.Println("[users 테이블이 존재하지 않아 새로 생성합니다..]")
+		createTableQuery := `
+	CREATE TABLE users (
+		user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		name VARCHAR(50) NOT NULL,
+		email VARCHAR(255) UNIQUE NOT NULL,
+		password_hash VARCHAR(255) NOT NULL, 
+		created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+		);`
+
+		_, err := db.Exec(createTableQuery)
+		if err != nil {
+			log.Fatalf("[users 테이블 생성에 실패하였습니다. %v]", err)
+		}
+		log.Println("[users 테이블 생성에 성공하였습니다.]")
+	} else {
+		log.Println("[users 테이블이 이미 존재합니다.]")
+	}
 }
